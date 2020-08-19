65a8c7eda71058daa2d66e976621b8a134dea7f8
koiski <koienrobin@gmail.com>
2020-08-19 11:58:56 +0200

    Update SvgMask.js
    Forgot more "," commas, fuck them

--- src/components/SvgMask.js
+++ src/components/SvgMask.js
@@ -153,8 +153,8 @@ export default function SvgMask({
           rx={rounded}
           width={width}
           height={height}
-          stroke={disableInteraction ? "none" : "#00c6ae"},
-          strokeWidth={4},
+          stroke={disableInteraction ? "none" : "#00c6ae"}
+          strokeWidth={4}
           pointerEvents={disableInteraction ? "auto" : "none"}
           fill="transparent"
           className={disableInteractionClassName}
